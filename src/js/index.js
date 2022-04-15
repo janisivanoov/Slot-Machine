9d81b0ae6e7dcc4463e036565f0ef0840d54c1a1
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2022-04-15 23:17:47 +0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import Slot from "./Slot.js";
+
+const config = {
+  inverted: false,
+  onSpinStart: (symbols) => {
+    console.log("onSpinStart", symbols);
+  },
+  onSpinEnd: (symbols) => {
+    console.log("onSpinEnd", symbols);
+  },
+};
+
+const slot = new Slot(document.getElementById("slot"), config);
